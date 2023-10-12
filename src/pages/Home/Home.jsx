610fa0af25910c31e161b12d544bf2534a2ca918
milanpaudell <milanpaudell31@gmail.com>
2023-10-12 21:00:37 +0545

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,39 @@
+import axios from "axios"
+import Navbar from "../../components/Navbar/Navbar"
+import { useEffect,useState } from "react"
+
+
+const Home = () => {
+  const [blogs,setBlogs] = useState([])
+  // api call here 
+
+  const fetchBlogs = async ()=>{
+   const response =  await axios.get('http://localhost:2000/blogs')
+   setBlogs(response.data.blogs)
+  }
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
+
+  
+  return (
+    <div>
+      <Navbar />
+      <div className="card" style={{width: '18rem'}}>
+    {blogs.map((blog)=>{
+      return(
+        <div key={blog._id} className="card-body">
+          <h5 className="card-title">{blog.title}</h5>
+          <h5 className="card-title">{blog.subTitle}</h5>
+          <p className="card-text">{blog.description}</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      )
+    }) }
+ 
+      </div>
+    </div>
+  )
+}
+
+export default Home
